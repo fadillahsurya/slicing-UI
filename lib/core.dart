faabd0404a4cf78d9afa2117baeaa6827c762f1e
fadillahsurya <fadillsfranzex@gmail.com>
2023-11-30 12:11:30 +0700

    Flutter

---
+++ lib/core.dart
@@ -0,0 +1,10 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:flutter_barbershop_ui/home.dart';
+export 'package:flutter_barbershop_ui/widget/barberman_widget.dart';
+export 'package:flutter_barbershop_ui/widget/date_widget.dart';
+export 'package:flutter_barbershop_ui/widget/service_widget.dart';
+export 'package:flutter_barbershop_ui/widget/time_widget.dart';
